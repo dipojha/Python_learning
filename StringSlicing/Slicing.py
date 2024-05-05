d5b8e2cd0a7f6396514d15ee9024dd6635eed07c
dipojha <dipojha03@gmail.com>
2024-05-05 13:09:01 +0545

    just to test

---
+++ StringSlicing/Slicing.py
@@ -0,0 +1,9 @@
+name="Dip Ojha"
+First_Name=name[0:3]
+Last_Name=name[4:8]
+Funky_Name=name[::2]
+Reversed_Name= name[::-1]
+print(First_Name)
+print(Last_Name)
+print(Funky_Name)
+print(Reversed_Name)
